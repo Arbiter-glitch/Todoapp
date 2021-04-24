c715b2ab50ccc3505e9de3428509f5ed5bb2ab3f
Arbiter-glitch <70324978+Arbiter-glitch@users.noreply.github.com>
2021-04-24 17:58:16 +0530

    Add files via upload

--- js/newtodo.js
+++ js/newtodo.js
@@ -10,7 +10,7 @@ function ajax(){
             for (var i=0;i<col.length-1;i++){
                  t+="  "+"<u>"+"  "+"<label style=padding-right:2px> </label>"+"<b>"+col[i]+"</b>"+"</u>";   
                  }      
-                 output=t+" "+"<b>"+"<label style=padding-left:82.64%>"+"<u>"+col[2]+"</u>"+"</label>"+"</b>"+"<p></p>"; 
+                 output=t+" "+"<b>"+"<label style=float:right>"+"<u>"+col[2]+"</u>"+"</label>"+"</b>"+"<p></p>"; 
             for(var i=0;i<response.length;i++){
                 if (response[i].completed==false){
                 output+="<p>"+ response[i].id+"."+" "+" <label style=padding-right:5px> </label>"+"<a style=text-decoration:none onMouseOver=this.style.color='#0F0'  onMouseOut=this.style.color='#000'>"+response[i].title+"</a>"+"<input style=float:right;padding-right:7px onchange=counting(this.checked) type=checkbox "+"</p>";
